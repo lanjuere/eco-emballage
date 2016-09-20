56ff50aefa37e57db79cc8970f9ad2c619afb471
lanjuere <lionel.anjuere@gmail.com>
2016-09-20 14:45:40 +0200

    [settings] Correction de l'implémentation de materialize-css

--- module-template/src/index.js
+++ module-template/src/index.js
@@ -1,5 +1,6 @@
 var angular = require('angular');
 
 require('materialize-css/sass/materialize.scss');
+require('materialize-css');
 var views = require('./app/views/views');
 angular.module('app', [views]);
