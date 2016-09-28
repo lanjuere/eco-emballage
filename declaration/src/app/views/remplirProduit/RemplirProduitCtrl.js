fe7935139110a57fe437f71e5af76610b917b010
lanjuere <lionel.anjuere@gmail.com>
2016-09-28 14:25:05 +0200

    [settings] Ajout des contrôles du formulaire

--- declaration/src/app/views/remplirProduit/RemplirProduitCtrl.js
+++ declaration/src/app/views/remplirProduit/RemplirProduitCtrl.js
@@ -2,4 +2,16 @@ module.exports = function () {
   this.typeDeclarations = ['Déclaration des UVC', 'Déclarations des cas spécifiques'];
   this.listMateriaux = ['ACIER - ALUMINIUM', 'PAPIER-CARTON', 'PLASTIQUE', 'VERRE - AUTRES'];
   this.actions = ['Action de réduction / recyclabilité'];
+  this.form = {};
+
+  this.ngClass = function(formValue){
+    ngClass = {};
+    ngClass['has-error'] = function(){return formValue.$invalid && !formValue.$pristine};
+    ngClass['has-success'] = function(){return formValue.$valid && !formValue.$pristine};
+    // return {
+    //   'has-error' : formValue.$invalid && !formValue.$pristine,
+    //   'has-success' : formValue.$valid
+    // ;}
+    return ngClass;
+  }
 };
